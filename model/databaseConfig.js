f9ebbef17cffc7b7fe047ac125e457ad466d4d06
jo3lbenjamin <joelbenjamin67@gmail.com>
2021-01-21 16:28:25 +0800

    First commit

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -4,7 +4,7 @@ var dbconnect = {
         var conn = mysql.createConnection({
             host: "localhost",
             user: "root",
-            password: "root1234",
+            password: "joelmysql",
             database: "islandfurniture-it07"
         });
         return conn;
